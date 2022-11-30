d986a1aa4e2b926ea778ec0054c67a8596571a7a
user <user@WIN-8I41D9CN0BC>
2022-11-30 23:33:04 +0300

    Добавьте файлы проекта.

---
+++ Task 7.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_7._2
+{
+    class Program
+    {
+        static void GetParamsCube(double a, out double s, out double v)
+        {
+            s = a * a;
+            v = a * a * a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double s;
+            double v;
+            GetParamsCube(a, out s, out v);
+            Console.WriteLine("Площадь поверхности куба {0:.00} Объем куба {1:.00}", s, v);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task7.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7._1
+{
+    class Program
+    {
+        static double GetSquare(double a, double b, double c)
+        {
+            double p = a + b + c;
+            return Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длины сторон первого треугольника");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double z1 = Convert.ToDouble(Console.ReadLine());
+            double S1 = GetSquare(x1, y1, z1);
+            Console.WriteLine("Введите длины сторон второготреугольника");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            double z2 = Convert.ToDouble(Console.ReadLine());
+            double S2 = GetSquare(x2, y2, z2);
+
+            if (S1 > S2)
+                Console.WriteLine("Площадь первого треугольника больше");
+            else
+                if (S1 < S2)
+                Console.WriteLine("Площадь второго треугольника больше");
+            else
+                Console.WriteLine("Площади равны");
+            Console.ReadKey();
+        }
+    }
+}
